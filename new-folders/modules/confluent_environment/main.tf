3deab55f399258e66987f506960ca43d0eb4f89e
Jeremy Ber <jber@confluent.io>
2025-06-27 16:03:40 -0500

    modularizing the correct way now

---
+++ new-folders/modules/confluent_environment/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    confluent = {
+      source  = "confluentinc/confluent" 
+      version = "2.22.0"                
+    }
+  }
+  }
+
+
+resource "confluent_environment" "my_environment" {
+  display_name = var.environment_name
+
+  stream_governance {
+    package = "ESSENTIALS"
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
