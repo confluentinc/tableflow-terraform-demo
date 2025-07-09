31babf43db62f3565fce1ed51149ccf7b3c0708e
Jeremy Ber <jber@confluent.io>
2025-07-09 14:21:25 -0500

    added snowflake catalog integration

--- new-folders/modules/snowflake_catalog_integration/main.tf
+++ new-folders/modules/snowflake_catalog_integration/main.tf
@@ -6,3 +6,28 @@ terraform {
     }
   }
 }
+
+resource "confluent_catalog_integration" "snowflake_catalog" {
+
+  environment {
+    id = var.environment_id
+  }
+  kafka_cluster {
+    id = var.kafka_cluster_id
+  }
+  display_name = "catalog-integration-snowflake"
+  snowflake {
+    endpoint       = var.snowflake_endpoint
+    warehouse      = var.snowflake_warehouse
+    allowed_scope  = var.snowflake_allowed_scope
+    client_id      = var.polaris_client_id
+    client_secret  = var.polaris_client_secret
+  }
+  credentials {
+    key    = var.tableflow_api_key
+    secret = var.tableflow_api_secret
+  }
+  lifecycle {
+    prevent_destroy = false
+  }
+}
