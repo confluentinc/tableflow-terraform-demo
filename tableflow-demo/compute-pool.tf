317d2abff4b180b3292f568f613ccd0e06a9e549
Jeremy Ber <jber@confluent.io>
2025-07-11 13:39:17 -0500

    moved folders out

--- tableflow-demo/compute-pool.tf
+++
@@ -1,9 +0,0 @@
-resource "confluent_flink_compute_pool" "my_compute_pool" {
-  display_name     = "flink_compute_pool"
-  cloud            = "AWS"
-  region           = var.aws_region
-  max_cfu          = 5
-  environment {
-    id = confluent_environment.my_environment.id
-  }
-}
